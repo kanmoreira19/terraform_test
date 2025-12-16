3b00bec845ae7d81027f31162cae3f36e307b7d9
kanmoreira19 <kanmoreira@gmail.com>
2025-12-16 18:13:07 -0300

    initial commit of Terraform configuration for Docker provider and resources

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.28"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8001
+  }
+}
